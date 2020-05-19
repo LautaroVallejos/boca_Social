bbdbf18f9044f126b2164b9a6ce58963418bb413
nicolas-carlini <nicolascarlini1@gmail.com>
2020-05-18 21:10:12 -0300

    blank for all states

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.use(require('./controllers/admin.crtl'));
-app.use(require('./controllers/auth.crtl'));
-app.use(require('./controllers/geo.crtl'));
-//app.use(require('./controllers/group.crtl'));
-app.use(require('./controllers/pay.crtl'));
-app.use(require('./controllers/points.ctrl'));
 //app.use(require('./controllers/'));
 
 module.exports = app;
