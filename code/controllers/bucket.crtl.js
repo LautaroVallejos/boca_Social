539946d8b1224a157977e5386d67d4fc316d6707
nicolas-carlini <nicolascarlini1@gmail.com>
2020-05-23 21:15:50 -0300

    docker

---
+++ code/controllers/bucket.crtl.js
@@ -0,0 +1,18 @@
+const {
+    Router
+} = require('express')
+const router = Router();
+
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const geoip = require('geoip-lite');
+
+const User = require('../models/User');
+
+router.get('/',(req, res)=>{
+    res.json({
+        microservice: "bucket"
+    })
+})
+
+module.exports = router;
--- unitTest/email.js
+++
@@ -1,5 +0,0 @@
-function validarEmail(valor) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(valor || "");
-}
-
-console.log(validarEmail("nicolascarlini@gmail.com"))
