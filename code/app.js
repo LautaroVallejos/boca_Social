44cf90d1490e747061c3a1b0b1e5c249b2c759ab
nicolas-carlini <nicolascarlini1@gmail.com>
2020-06-01 09:54:45 -0300

    segurity

--- code/app.js
+++ code/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//security
+app.disable('x-powered-by')
+app.use(helmet());
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+app.set('trust proxy', 1);
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({
